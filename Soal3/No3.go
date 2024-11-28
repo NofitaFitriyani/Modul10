93dc1dfc85f3abe826a863051b5b011992d9aaf8
NofitaFitriyani <2311102001@ittelkom-pwt.ac.id>
2024-11-28 18:15:10 +0700

    Modul10

---
+++ Soal3/No3.go
@@ -0,0 +1,42 @@
+package main
+//2311102001_NofitaFitriyani
+import "fmt"
+
+func hitungMinMax(arr []float64) (min, max float64) {
+	min, max = arr[0], arr[0]
+	for _, berat := range arr {
+		if berat < min {
+			min = berat
+		}
+		if berat > max {
+			max = berat
+		}
+	}
+	return
+}
+
+func hitungRerata(arr []float64) (total float64) {
+	for _, berat := range arr {
+		total += berat
+	}
+	return total / float64(len(arr))
+}
+
+func main() {
+	var n int
+	fmt.Print("Masukkan banyak data berat balita: ")
+	fmt.Scan(&n)
+
+	arrBalita := make([]float64, n)
+	for i := 0; i < n; i++ {
+		fmt.Printf("Masukkan berat balita ke-%d: ", i+1)
+		fmt.Scan(&arrBalita[i])
+	}
+
+	bMin, bMax := hitungMinMax(arrBalita)
+	rerata := hitungRerata(arrBalita)
+
+	fmt.Printf("\nBerat balita minimum: %.2f kg\n", bMin)
+	fmt.Printf("Berat balita maksimum: %.2f kg\n", bMax)
+	fmt.Printf("Rerata berat balita: %.2f kg\n", rerata)
+}
